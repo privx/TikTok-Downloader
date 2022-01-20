72603166457829acbd54092c3aff053c2a344f10
Bima Gates <40799459+privx@users.noreply.github.com>
2022-01-21 05:17:41 +0700

    Update download.php

--- download.php
+++ download.php
@@ -4,18 +4,22 @@ error_reporting(0);
 
 echo "[?] url: ";
 $url = trim(fgets(STDIN))?: exit("null");
+echo "[?] file type [a. mp4 b. mp3]: ";
+$type = trim(fgets(STDIN))?: exit("null");
+$ext = ($type == "a") ? "mp4" : "mp3";
 
-$temp_fileName = "tikvids/vid_".md5($url).".mp4";
-$temp_video = getVideo($url);
-$fileName = write_to_file($temp_video,$temp_fileName);
+$temp_fileName = "tikvids/vid_".md5(uniqid()).$ext;
+$temp_video = getVideo($url, $ext);
+$fileName = saveFile($temp_video,$temp_fileName);
 (filesize($temp_fileName) > 0)? print("[>] File saved! on ".$temp_fileName) : unlink($temp_fileName);
 
-function getVideo($url,$timeout = 60){
+function getVideo($url,$type,$timeout = 60){
     $ch = curl_init();
     $startTime = time();
-    curl_setopt($ch, CURLOPT_URL, "http://34.125.190.190/APl/tikurl.php?tu=".$url);
+    curl_setopt($ch, CURLOPT_URL, "http://34.125.190.190/APl/tikurl2.php?tu=".$url."&op=".$type);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+    curl_setopt($ch, CURLOPT_AUTOREFERER, false);
     curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
     $result = curl_exec($ch);
     if(time() > $startTime + $timeout) exit("[!] Timeout");
@@ -25,9 +29,9 @@ function getVideo($url,$timeout = 60){
     return($result);
 }
 
-function write_to_file($text,$new_filename){
+function saveFile($text,$filename){
     if(!is_dir("tikvids")) mkdir("tikvids", 0777, true);
-    $fp = fopen($new_filename, 'w');
+    $fp = fopen($filename, 'w');
     fwrite($fp, $text);
     fclose($fp);
 }
